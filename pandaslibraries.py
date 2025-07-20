bb42d5f12376d59b28d8de9953a69ad478e45909
subha1405 <129583454+subha1405@users.noreply.github.com>
2025-07-20 22:29:35 +0530

    Update pandaslibraries.py

--- pandaslibraries.py
+++ pandaslibraries.py
@@ -1,21 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-# df=pd.DataFrame(
-#     {
-#         "NAME":["abdul","Sankar","Rajneesh","abdul","Sankar","Rajneesh","abdul","Sankar","Rajneesh","abdul","Sankar","Rajneesh"],
-#         "SEX":["MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE"]
-#     }
-# )
-# print(df.head(1))
-# print(df.tail(9))
-
-
+df=pd.DataFrame({"NAME":["abdul","Sankar","Rajneesh","abdul","Sankar","Rajneesh","abdul","Sankar","Rajneesh","abdul","Sankar","Rajneesh"],
+         "SEX":["MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE","MALE"]})
+print(df.head(9))
+print(df.tail(9))
+plt.plot(df["NAME"],df["SEX"])
+plt.show()
 # Reading a csv file
-studentscorelist=pd.read_csv("studentlist.csv")
-print(studentscorelist.head())
-print(studentscorelist[["AGE","SCORE"]].describe())
-studentscorelist.plot.scatter(x="AGE",y="SCORE")
-# plt.xlabel("AGE")
-# plt.ylabel("SCORE")
-# plt.title("SCORECARD")
-# plt.show()
+#studentscorelist=pd.read_csv("studentlist.csv")
+#print(studentscorelist.head())
+#print(studentscorelist[["AGE","SCORE"]].describe())
+#studentscorelist.plot.scatter(x="AGE",y="SCORE")
+#plt.xlabel("AGE")
+#plt.ylabel("SCORE")
+#plt.title("SCORECARD")
+#plt.show()
